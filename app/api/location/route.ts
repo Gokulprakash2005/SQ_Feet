673e1aeb9c8b5c40382f9220b8d4a3117ed70c6f
Gokulprakash2005 <gokulprakash1301@gmail.com>
2025-11-26 00:09:58 +0530

    fetch update

--- app/api/location/route.ts
+++ app/api/location/route.ts
@@ -37,6 +37,7 @@ export async function POST(request: NextRequest) {
     
     const location = await prisma.location.create({
       data: {
+        id: parseInt(data.id),
         address: data.address,
         city: data.city,
         pincode: parseInt(data.pincode)
